f606f1aed1a5a1cc48ac7ea9dc50b430c8eda8e9
Velocityy19 <114207378+Velocityy19@users.noreply.github.com>
2025-11-11 13:45:29 +0530

    Update OnlineQuizApplication.java

--- src/main/java/com/example/Online/Quiz/OnlineQuizApplication.java
+++ src/main/java/com/example/Online/Quiz/OnlineQuizApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@EnableAsync
 @SpringBootApplication
 public class OnlineQuizApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineQuizApplication.class, args);
 	}
